bef25ea5275ddae5e566529f955e4f067c51c0ef
khpark <dev.keonhee@gmail.com>
2023-09-04 16:04:42 +0900

    Fix Remove - Remove Pivot이 정상 적용되지 않는 버그 수정

--- Assets/Runtime/Scripts/PivotOnClickManager.cs
+++ Assets/Runtime/Scripts/PivotOnClickManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -8,11 +9,14 @@ namespace foveated.sample
     public class PivotOnClickManager : MonoBehaviour, IPointerClickHandler
     {
         FoveatedRenderingManager FoveatedRenderingManager => FoveatedRenderingManager.Instance;
+        Vector2 pivotPosition = Vector2.zero;
+        public Vector2 PivotPosition { set => pivotPosition = value; }
 
         public void OnPointerClick(PointerEventData eventData)
         {
             if (FoveatedRenderingManager.State == FRMState.Remove)
             {
+                FoveatedRenderingManager.CameraShader.RemoveAnisotropyPivot(this, pivotPosition);
                 FoveatedRenderingManager.RemovePivotInPool(this);
                 Destroy(this.gameObject);
             }
